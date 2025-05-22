23abbe757400c1f5f8a90f8f0b8e75d9be71dad7
Aaron <aaronlimfz@gmail.com>
2025-05-22 14:40:02 +0800

    Add set of available avatars

--- src/config/scenarios.ts
+++ src/config/scenarios.ts
@@ -1,4 +1,3 @@
-
 export interface Scenario {
   id: string;
   title: string;
@@ -18,9 +17,10 @@ export const scenarios: Scenario[] = [
     description: 'Enhance your insurance sales skills through interactive training, learn to effectively showcase policies, and practice providing real-time support in a virtual insurance consultation setting!',
     href: '/temus-avatar-customer',
     startMessage: 'Hello there.',
-    prompt: 'You are role-playing as a Temus Avatar Customer. You are a customer who is looking for life insurance.',
-    avatar: 'henry',
-    backgroundImageUrl: 'https://files.123freevectors.com/wp-content/solidbackground/gilded-beige-free-solidcolor-background.jpg',
-    voice: 'henry'
-  }
+    // prompt: 'You are role-playing as a Temus Avatar Customer. You are a customer who is looking for life insurance. Please always keep your responses concise and to the point. At most, you should only respond with 2-3 sentences.',
+    prompt: "You are role-playing as an expert in Coffee. You are given a question and you need to answer it in a way that is helpful and informative. Please always keep your responses concise and to the point.",
+    avatar: 'michelle',
+    backgroundImageUrl: 'https://images.pexels.com/photos/683039/pexels-photo-683039.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    voice: 'michelle'
+  },
 ]; 
